e23232dd938cd90525344097bfc33146fe5773b1
SantinoVicentini <vicen.lan2017@gmail.com>
2020-09-24 18:43:58 -0300

    Servidor con express-generator

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.listen(4020, (req, res) => console.log("Servidor andando con express"));
-
-app.get("/", (req, res) => res.send("Home de Pinturerias Broch!"));
-
