07f1505544a7210135b00e59119af7f1badb7b95
SantinoVicentini <vicen.lan2017@gmail.com>
2020-11-12 16:39:19 -0300

    exteriores json-prueba

--- pintureriaBroch/controllers/indexController.js
+++ pintureriaBroch/controllers/indexController.js
@@ -1,3 +1,9 @@
+const path = require('path');
+const fs = require ('fs');
+
+let datosJson = fs.readFileSync(path.resolve(__dirname,"../data/exteriores.json"),"utf-8");
+let datosExteriores = JSON.parse(datosJson);
+
 const indexController = {
     index: function(req, res, next) {
         res.render('index');
@@ -19,10 +25,21 @@ const indexController = {
     },
     productAdd: function(req, res, next) {
         res.render('productAdd');
-    }
+    },
+    id: function(req,res,next){
+        let id = req.params.id;
+        let exteriores = {};
+        for(let i = 0; i <datosExteriores.length; i ++){
+            if(datosExteriores[i].id == id){
+                exteriores = datosExteriores[i];
+                return res.render ("exteriores", { exteriores, error: ""});          
+        }
 
-      
+    }  
 
+    return res.render ("exteriores", {error : "no se encontro el producto ", id});  
+
+}  
 }
 
 module.exports = indexController;
