9c544261b8e5f4fb68300ab5399c78ee9e821082
Alexis Coria <coria.alexis94@gmail.com>
2020-11-29 02:53:02 -0300

    CRUD:_create_y_edit

--- pintureriaBroch/controllers/productController.js
+++ pintureriaBroch/controllers/productController.js
@@ -1,13 +1,37 @@
+const fs = require('fs');
+var products = JSON.parse(fs.readFileSync(__dirname + '/../data/products.json'));
+
+
 const productController = {
     listado: function(req, res, next) {
         res.send('Lsitado de todos los productos');
     },
-    crear: function(req, res, next) {
+    create: function(req, res, next) {
         res.render('productAdd');
     },
     eliminar: function(req, res, next) {
         res.send('Eliminar un producto');
-    }
+    },
+    store: function(req,res,next){
+        products.push(req.body);
+        let productsJSON = JSON.stringify(products);
+        fs.writeFileSync(__dirname + '/../data/products.json', productsJSON);
+        return res.send('Producto creado');
+    },
+    edit: function(req,res,next){
+        let idProduct = req.params.id;
+        var productFound;
+       for (let i=0; i<products.length;i++){
+           if(products[i].id == idProduct){
+               productFound = products[i];
+               break;
+           }
+        }
 
+        if(productFound){
+            res.render("edit",{productFound});
+        }else{
+        return res.send('Usuario invalido');}
+    }
 }
 module.exports = productController;
--- pintureriaBroch/routes/products.js
+++ pintureriaBroch/routes/products.js
@@ -4,7 +4,10 @@ var router = express.Router();
 const productController = require('../controllers/productController')
 
 router.get('/listado',productController.listado);
-router.get('/crear',productController.crear);
+router.get('/create',productController.create);
+router.post('/',productController.store);
+router.get('/:id',productController.edit);
+router.get('/:id/edit',productController.create);
 router.get('/eliminar',productController.eliminar);
 
 module.exports = router;
