f87a2dda2f06078bc0b40f9c79265af3138d6e11
Alexis Coria <coria.alexis94@gmail.com>
2020-12-13 22:12:27 -0300

    CRUD_create_user

--- pintureriaBroch/controllers/userController.js
+++ pintureriaBroch/controllers/userController.js
@@ -1,3 +1,8 @@
+/*let bcrypt = require('bcrypt');*/
+const fs = require('fs');
+var users = JSON.parse(fs.readFileSync(__dirname + '/../data/users.json'));
+
+
 const userController = {
     login: function(req, res, next) {
         res.render('login');
@@ -6,16 +11,20 @@ const userController = {
         res.render('register');
     },
     perfil: function(req, res, next) {
-        res.send('lEste es mi perfil');
+        res.send('Este es mi perfil');
     },
-    create: function(req, res, next) {
+    create: function(req,res,next){
+        users.push(req.body);
+        let usersJSON = JSON.stringify(users);
+        fs.writeFileSync(__dirname + '/../data/users.json', usersJSON);
+        return res.send('Usuario creado'); }/*function(req, res, next) {
         let usuarioIngresado = {
             nombre: req.body.fullName,
             apellido: req.body.username,
             email: req.body.email
         }
-        res.send(usuarioIngresado);/*Acá iría un posible redireccionamiento*/
-    },
+        res.send(usuarioIngresado);/*Acá iría un posible redireccionamiento
+    }*/,
     edit: function(req, res, next) {
         let idUser = req.params.idUser;
 let users = [
