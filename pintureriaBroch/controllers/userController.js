ded8cab35006848734d88b7b191074c56c7a6505
Alexis Coria <coria.alexis94@gmail.com>
2020-12-17 20:32:53 -0300

    Me_olvide_agregar_los_archivos

--- pintureriaBroch/controllers/userController.js
+++ pintureriaBroch/controllers/userController.js
@@ -1,4 +1,4 @@
-/*let bcrypt = require('bcrypt');*/
+let bcrypt = require('bcryptjs');
 const fs = require('fs');
 var users = JSON.parse(fs.readFileSync(__dirname + '/../data/users.json'));
 
@@ -14,27 +14,18 @@ const userController = {
         res.send('Este es mi perfil');
     },
     create: function(req,res,next){
-        users.push(req.body);
+        var users1 = {
+            first_name: req.body.fullName,
+            last_name: req.body.username,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password,10),
+            avatar: req.body.avatar
+        };
+        users.push(users1);
+       
         let usersJSON = JSON.stringify(users);
         fs.writeFileSync(__dirname + '/../data/users.json', usersJSON);
-        return res.send('Usuario creado'); }/*function(req, res, next) {
-        let usuarioIngresado = {
-            nombre: req.body.fullName,
-            apellido: req.body.username,
-            email: req.body.email
-        }
-        res.send(usuarioIngresado);/*Acá iría un posible redireccionamiento
-    }*/,
-    edit: function(req, res, next) {
-        let idUser = req.params.idUser;
-let users = [
-    {id: 1, name: 'Santino'},
-    {id: 2, name: 'Luciano'},
-    {id: 3, name: 'Alexis'}
-];
-        let userToEdit = users[idUser];
-
-        res.render("userEdit",{userToEdit: userToEdit});
-    },
+        return res.redirect('/'); }
+    
 }
 module.exports = userController;
--- pintureriaBroch/routes/users.js
+++ pintureriaBroch/routes/users.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/userController');
+let multer = require('multer');
+let path = require ('path');
 
-const userController = require('../controllers/userController')
 
 router.get('/login',userController.login);
 router.get('/register',userController.register);
-router.post('/register',userController.create);
+router.post('/register',upload.any(),userController.create);
 router.get('/perfil',userController.perfil);
-router.get('/edit/:idUser',userController.edit);
+/*router.get('/edit/:idUser',userController.edit);
 router.put('/edit', function(req, res, next) {
   res.send('FUI POR PUT');
-});
+});*/
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
