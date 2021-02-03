516192336f253195839be46f6387a0a26e5c1ed2
Luciano Serenellini <serenelliniluciano@gmail.com>
2021-02-03 20:58:15 -0300

    correcion base de datos

--- pintureriaBroch/database/config/config.js
+++ pintureriaBroch/database/config/config.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "root",
-    "database": "pintureriaBroch",
-    "host": "127.0.0.1",
-    "port": "8889",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS || null,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "test": {
