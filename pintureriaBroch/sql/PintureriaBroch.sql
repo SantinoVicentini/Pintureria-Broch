2628c1a386fd455ae4a2b1584fb5635e7515a751
Alexis Coria <coria.alexis94@gmail.com>
2021-03-07 00:07:33 -0300

    cambios en BD

---
+++ pintureriaBroch/sql/PintureriaBroch.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE pintureriaBroch;
+USE `pintureriaBroch`;
+
+CREATE TABLE user(
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(50) NOT NULL,
+userName VARCHAR (50) NOT NULL UNIQUE,
+email VARCHAR(50) NOT NULL UNIQUE,
+password VARCHAR(60) NOT NULL,
+avatar VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE pintureriaBroch.category(
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(50) NOT NULL);
+
+CREATE TABLE pintureriaBroch.trademark(
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(50) NOT NULL);
+
+CREATE TABLE pintureriaBroch.color(
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(50) NOT NULL);
+
+CREATE TABLE product(
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(50) NOT NULL,
+description text NOT NULL,
+price INT NOT NULL,
+image VARCHAR(50) NOT NULL,
+category_id INT NOT NULL,
+color_id INT NOT NULL,
+trademark_id INT NOT NULL
+);
+
+DROP TABLE IF EXISTS pintureriaBroch.cart;
+
+CREATE TABLE pintureriaBroch.cart(
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+total VARCHAR(50) NOT NULL,
+status tinyint NOT NULL
+);
+
+CREATE TABLE pintureriaBroch.cart_product(
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL
+);
