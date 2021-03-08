577579f127530ccdf9fe8d57c69dba439863b50b
Alexis Coria <coria.alexis94@gmail.com>
2021-03-08 18:06:50 -0300

    comienzo carrito

--- pintureriaBroch/controllers/cartController.js
+++ pintureriaBroch/controllers/cartController.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const { sequelize } = require('../database/models');
 const db = require("../database/models");
 let {check, validationResult,body}= require('express-validator');
+const { info } = require('console');
 
 
 const cartController = {
@@ -9,26 +10,63 @@ const cartController = {
 addProduct:function(req, res,next) {
 console.log(req.params.id);
 res.send("Llego algo")
-    /*db.Cart.create({
-        user_id: req.session.userid
-    }).then(function(result){
-        db.Cart_Product.create({
-            cart_id: result.id,
-            product_id: req.params.id
-        })
-        db.Product.findByPk(req.params.id)
-        .then(function(product){
-            db.Cart.update({total: product.price}, {
-                where: {
-                  user_id: req.session.userid
+            /*Ponemos el id del usuario que quiere agregar un prodcuto al carrito*/
+             var userid = req.session.userLogged.id;
+
+             /*Si el carrito esta activo. Esto es para una persona que ya se le creo un carrito*/
+
+
+            db.Cart.findOne({
+                where:
+                {
+                    user_id: userid,
+                    status:"activo"
                 }
-            })})
+            }).then(resp=>{         
+                 if(resp){   /*ALGO CON CART PRODUCT*/ }
+                 else{
+                    /*creamos el carrito*/
+                 }
+                 db.Cart.create(
+                    {
+                        user_id : userId,
+                        status: "activo"
+                    })
+                .then(resp => {
+                    /* Agregamos el producto al nuevo carrito creado*/
 
-    }) */
+                    req.session.carrito_id = response.id;
+                    db.CartProduct.create(
+                        {
+                            carrito_id : resp.id,
+                            producto_id : req.body,/*el id del producto*/
+                        })
+                    .then((data) => {
+                        return res.json(data)
+                    })
+                })      
+            })
+            },
+deleteProductCart:function(req, res,next) {
+},
+viewCart : function(req, res, next){
+    db.Cart.findOne({
+        where:
+        {
+            user_id: userid,
+            status:"activo"
+        },
+    })
+    .then( info => {
+        if(info){
+        req.session.carrito_id = info.id;
+        res.render("/cart", {info: info})
+        } else {
+            res.render("/cart")
+        }
+    })
+
+},
 }
-
-}
-
-
 
 module.exports = cartController;
--- pintureriaBroch/routes/carts.js
+++ pintureriaBroch/routes/carts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const cartController = require('../controllers/cartController');
 
-
+router.get('/', cartController.viewCart);
 router.post('/addProduct/:id', cartController.addProduct);
 
 router.get('/deleteProduct/:id', cartController.deleteProductFromCart);
