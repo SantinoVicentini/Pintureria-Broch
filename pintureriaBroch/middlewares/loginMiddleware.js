18808f492f1680c180b115983e4347ef6fde1b8b
Alexis Coria <coria.alexis94@gmail.com>
2021-03-14 14:05:43 -0300

    responsive home, login, perfil usuario

--- pintureriaBroch/middlewares/loginMiddleware.js
+++ pintureriaBroch/middlewares/loginMiddleware.js
@@ -2,7 +2,7 @@ function loginMiddleware(req,res,next){
     if ( req.session.userLogged != undefined){
         next();
       }else{
-          res.send('Solo para usuarios');
+        res.render("error",{errors:[{msg:'Solamente los usuarios pueden ver esta pagina'}]});
 }
 }
 module.exports = loginMiddleware;
